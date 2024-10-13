1f94a7702b9a561c270b9424b23b2d97ac862a9d
Arnavya <arnavyachettri@gmail.com>
2024-10-13 18:46:44 +0530

    Enabled JPA Auditing and added entity listeners to automatically update 'createdAt' and 'lastModifiedAt' fields

--- src/main/java/com/sst/bookmyshowsst/BookMyShowSstApplication.java
+++ src/main/java/com/sst/bookmyshowsst/BookMyShowSstApplication.java
@@ -2,8 +2,10 @@ package com.sst.bookmyshowsst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookMyShowSstApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,9 @@ public class BookMyShowSstApplication {
     }
 
 }
+/*
+Steps to automatically add createAt & lastModifiedAt columns.(Auditing attributes).
+i)Enable JPA Auditing at SpringBoot application.
+ii)Add this annotation "@EntityListeners(AuditingEntityListener.class)" on BaseModelClass
+iii) Use @createdDate, @LastModifiedDate annotations on the respective attributes.
+ */
--- src/main/java/com/sst/bookmyshowsst/models/BaseModel.java
+++ src/main/java/com/sst/bookmyshowsst/models/BaseModel.java
@@ -1,20 +1,25 @@
 package com.sst.bookmyshowsst.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)//role -> if any event happen on those attributes with (@CreatedDate, @LastModifiedDate) annotations keep a track of those events.
 public class BaseModel {
     @Id      //used to define PK
     @GeneratedValue(strategy = GenerationType.IDENTITY) // for auto incrementation of PK.
     private Long id;
+
+    //Auditing attributes
+    @CreatedDate // means whenever we create an object in the database this value(createdAt) will take the current timestamp & gets saved in the database. & @EntityListner() will start listning.
     private Date createdAt;
+    @LastModifiedDate//whenever we are performing modification over this data this value(lastModifiedAt) will get updated with the current timestamp as @EventListner is listning and will be saved in automatically.
     private Date lastModifiedAt;
 }
